18809394673e8999eaddfb8c351c04ae6198d9df
Robert <robert.mabry86@gmail.com>
2023-04-15 10:31:21 -0500

    replaced code with solution, added comments to understand later

--- components/JokeGenny.js
+++ components/JokeGenny.js
@@ -1,27 +1,20 @@
-import React, { useState } from 'react';
-import getJoke from '../api/jokeData';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-
-const JokeGenny= () => {
-  const [currentJoke, setCurrentJoke] = useState('');
-
-  const generateJoke = async () => {
-    try {
-      const response = await fetch({getJoke});
-      const data = await response.json();
-      setCurrentJoke(`${data.setup} ${data.punchline}`);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
+//this component is exporting a func that will call the api, then apply that info to a btn click
+export default function Joker({ joke, btnText }) {
   return (
     <>
-      <h1>Joke Generator</h1>
-      <button onClick={generateJoke}>Generate Joke</button>
-      {currentJoke && <p>{currentJoke}</p>}
+      <h1>{joke.setup}</h1>
+      <p>{btnText !== 'Get Punchline' ? joke.punchline : ''}</p>
     </>
   );
+}
+//
+Joker.propTypes = {
+  joke: PropTypes.shape({
+    setup: PropTypes.string,
+    punchline: PropTypes.string,
+  }).isRequired,
+  btnText: PropTypes.string.isRequired,
 };
-
-export default JokeGenny;
